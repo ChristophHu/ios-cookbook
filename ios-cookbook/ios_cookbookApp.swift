6e5a88f0d44266807f1f5c2a24677590b9826a7f
christophhu <christoph.huschenhoefer@gmx.de>
2025-10-15 11:23:55 +0200

    Initial Commit

---
+++ ios-cookbook/ios_cookbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_cookbookApp.swift
+//  ios-cookbook
+//
+//  Created by Christoph Huschenhöfer on 15.10.25.
+//
+
+import SwiftUI
+
+@main
+struct ios_cookbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
